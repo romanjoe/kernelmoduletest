ff1c172fedf9fc76caa9824378a0e0a02bbe00e8
romanjoe <mrromanjoe@gmail.com>
2014-03-21 00:02:10 +0200

    Initial commit for work with kernel modules

---
+++ Makefile
@@ -0,0 +1,21 @@
+CURRENT = $(shell uname -r)
+KDIR = /lib/modules/$(CURRENT)/build
+PWD = $(shell pwd)
+TARGET1 = md1
+TARGET2 = md2
+TARGET3 = md3
+TARGET4 = log_level
+
+obj-m	:= $(TARGET1).o $(TARGET2).o $(TARGET3).o $(TARGET4).o
+
+default:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+
+clean:
+	@rm -f *.o *.cmd *.flags *.mod.c *.order
+	@rm -f .*.*.cmd *~ *.*~ TODO.*
+	@rm -fR .tmp*
+	@rm -rt .tmp_versions
+
+disclean: clean
+	@rm *.ko *.symvers
---
+++ log_level.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+
+static const char* levl[] = {
+   KERN_DEBUG, KERN_INFO, KERN_NOTICE,
+   KERN_WARNING, KERN_ERR, KERN_CRIT, KERN_ALERT,
+//  KERN_EMERG   
+};
+
+static int __init log_init( void ) {
+   char out[ 80 ];
+   int i;
+   for( i = 0; i < sizeof( levl ) / sizeof( levl[ 0 ] ); i++ ) {
+      sprintf( out, "%smessage level: %s\n", levl[ i ], levl[ i] ); 
+      printk( out );
+   }
+   return -1;
+}
+
+module_init( log_init );
